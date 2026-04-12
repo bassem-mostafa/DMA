fe0aaa0319ced1a65be4405b28a5082aeeb160fc
Bassem Mostafa <47607700+bassem-mostafa@users.noreply.github.com>
2026-04-12 18:52:50 +0200

    version 0.0.0.v20260412-1852

--- DMA_Internal.h
+++ DMA_Internal.h
@@ -88,7 +88,7 @@ extern "C"
     // #### Public Type(s) #########################################################
     // #############################################################################
 
-    typedef struct DMA_Instance_Context_t DMA_Instance_Context_t;
+    typedef struct DMA_InstanceContext_t DMA_InstanceContext_t;
 
     typedef struct DMA_Instance
     {
@@ -96,7 +96,7 @@ extern "C"
 
         union
         {
-            DMA_Instance_Context_t * Context;
+            DMA_InstanceContext_t * Context;
             DMA_STM32L496VGT6P_Instance_t * STM32L496VGT6P;
         };
     } DMA_Instance_t;
